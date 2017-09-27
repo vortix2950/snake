8d3e61afc725693cc74ca7f71a70848c8ce85b88
Nathan Bean <nhbean@ksu.edu>
2017-09-27 11:36:15 -0500

    In-class work9-27

--- src/game.js
+++ src/game.js
@@ -7,19 +7,50 @@ import Snake from './snake';
   */
 export default class Game {
   constructor() {
-    this.snake = new Snake();
+    this.snake = new Snake(50, 50, 3);
     this.food = [];
-    // Create the canvas
-    this.canvas = document.createElement('canvas');
-    this.canvas.width = 100;
-    this.canvas.height = 100;
-    document.body.appendChild(this.canvas);
-    this.context = this.canvas.getContext('2d');
+    this.over = false;
+    // Create the back buffer canvas
+    this.backBufferCanvas = document.createElement('canvas');
+    this.backBufferCanvas.width = 100;
+    this.backBufferCanvas.height = 100;
+    this.backBufferContext = this.backBufferCanvas.getContext('2d');
+    // Create the screen buffer canvas
+    this.screenBufferCanvas = document.createElement('canvas');
+    this.screenBufferCanvas.width = 100;
+    this.screenBufferCanvas.height = 100;
+    document.body.appendChild(this.screenBufferCanvas);
+    this.screenBufferContext = this.screenBufferCanvas.getContext('2d');
+    // Bind class functions
+    this.update = this.update.bind(this);
+    this.render = this.render.bind(this);
+    this.loop = this.loop.bind(this);
+    // Start the game loop
+    this.interval = setInterval(this.loop, 500);
   }
+  /** @method update
+    * Updates the game world.
+    */
   update() {
-    this.snake.update();
+    this.food.forEach((food) => {
+      food.update();
+    });
+    this.over = this.snake.update();
   }
+  /** @method render
+    * Renders the game world
+    */
   render() {
-    this.snake.render(this.context);
+    this.backBufferContext.fillStyle = '#ccc';
+    this.backBufferContext.fillRect(0, 0, 100, 100);
+    this.food.forEach((food) => {
+      food.render(this.backBufferContext);
+    })
+    this.snake.render(this.backBufferContext);
+    this.screenBufferContext.drawImage(this.backBufferCanvas,0,0)
+  }
+  loop() {
+    this.update();
+    this.render();
   }
 }
--- src/snake.js
+++ src/snake.js
@@ -13,12 +13,39 @@ export default class Snake {
       });
     }
     this.direction = 'right';
+    // bind class methods
+    this.update = this.update.bind(this);
+    this.render = this.render.bind(this);
   }
   update() {
+    var x = this.body[0].x;
+    var y = this.body[0].y;
+    switch(this.direction) {
+      case 'right':
+        x++;
+        break;
+      case 'left':
+        x--;
+        break;
+      case 'up':
+        y--;
+        break;
+      case 'down':
+        y++;
+        break;
+    }
     // Did we smack a wall?
+    // If we move off-board, game is over
+    if(x < 0 || x > this.width || y < 0 || y > this.height)
+      return true;
+
+    // Move the snake
+    this.body.pop();
+    this.body.unshift({x: x, y: y});
     // Did we eat ourselves?
     // Did we eat food?
     // Do we need to grow?
+
   }
   /** @function render
     * Render the snake
